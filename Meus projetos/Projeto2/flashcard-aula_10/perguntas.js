3bbedd2143f3f2b1e959aa00f79afd6109771800
Supernova-1QI <140723076+Supernova-1QI@users.noreply.github.com>
2024-10-28 07:57:49 -0300

    Add files via upload

---
+++ Meus projetos/Projeto2/flashcard-aula_10/perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'História',
+    'Quando começou a 2 Guerra Mundial?',
+    'Foi no dia 1 de Setembro de 1939.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    'O brasil tem 26 estados, além do DF'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 49?',
+    'A raiz quadrada de 49 é 7'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz fraco em inglês?',
+    'Fraco em ingles é weak (UEAK)'
+)
+criaCartao(
+    'Biologia',
+    'Qual é o maior orgâo humano?',
+    'O maior orgão humano é a pele!'
+)
+criaCartao(
+    'Física',
+    'Qual e a formula da 3 lei de Newton?',
+    'A fórmula é F = m . a (Força é igual a massa vezes aceleração).'
+)
